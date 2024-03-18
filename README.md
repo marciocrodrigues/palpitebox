cdd9be56a7cc35b9f3a7e686f966cfcaed4615c6
marciocostarodrigues <marciodeath@hotmail.com>
2024-03-18 17:16:15 -0300

    Alterado para utilizar arquivo env

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # palpitebox
 
 Aplicação criada no curso fullstake do devpleno
+
+Acrescentar no arquivo .env as chaves da api do google sheet email e o identificador da planilha, utilizar como login contas de serviço
--- google-sheet-config/google-sheet-config.js
+++ google-sheet-config/google-sheet-config.js
@@ -1,10 +1,12 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
-import credentials from './credentials.json'
 
-const doc = new GoogleSpreadsheet('16Ef3lPhr2ZNVbbj8j0DtzuC6guIEk_nrV7QHuyn6O9Q')
+const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
 export const docConfig = async () => {
-    await doc.useServiceAccountAuth(credentials)
+    await doc.useServiceAccountAuth({
+        client_email: process.env.SHEET_CLIENT_EMAIL,
+        private_key: process.env.SHEET_PRIVATE_KEY,
+    })
     await doc.loadInfo()
 
     return doc
